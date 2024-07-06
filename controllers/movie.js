a5d5ba195dcd0ed13bd5032d2024105a73499fec
eric hildevert <eric.hildebert@ext.bpifrance.fr>
2024-07-06 23:13:55 +0200

    Adding db connexion. Adding controller logic

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,21 +1,35 @@
 
+const Movie = require('./../models/movie');
 
 exports.createMovie = (req, res, next) => {
-    res.status(200).json({ message: 'Create Movie !'});
+    const movie = new Movie({
+        ...req.body
+    });
+    movie.save()
+    .then( () => { res.status(200).json({ message: 'Objet enregistré' }) } )
+    .catch( (error) => {res.status(400).json({ error})} );
 };
 
 exports.getMovies = (req, res, next) => {
-    res.status(200).json({ message: 'Get All Movies !'});
+    Movie.find()
+    .then( (movies) => { res.status(200).json({ movies })} )
+    .catch( (error) => {res.status(400).json({ error })} );
 };
 
 exports.getOneMovie = (req, res, next) => {
-    res.status(200).json({ message: 'Get One Movie !'});
+    Movie.findOne({ _id: req.params.id })
+    .then( (product) => { res.status(200).json({ product }) })
+    .catch( (error) => { res.status(404).json(error) });
 };
 
 exports.updateMovie = (req, res, next) => {
-    res.status(200).json({ message: 'Update One Movie !'});
+    Movie.updateOne({ _id: req.params.id }, { ...req.body, _id:req.params.id })
+    .then( () => { res.status(200).json({ message: 'Objet mis à jour'})})
+    .catch( (error) => { res.status(400).json(error) });
 };
 
 exports.deleteMovie = (req, res, next) => {
-    res.status(200).json({ message: 'Delete Movie !'});
+    Movie.deleteOne({ _id: req.params.id })
+    .then( () => { res.status(200).json({ message: 'Objet supprimé' }) })
+    .catch( (error) => { res.status(400).json(error) });
 };
--- express.js
+++ express.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const movieRoutes = require('./routes/movie');
+const mongoose = require('mongoose');
 
 const app = express();
 
-const movieRoutes = require('./routes/movie');
+mongoose.connect('mongodb+srv://praline:praline@cluster0.6hdenaz.mongodb.net/', {
+    usenewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then( () => { console.log('Connexion à MongoDB réussie !') })
+.catch( () => { console.log('Connexion à MongoDB échouée !') });
+
+app.use( (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(express.json());
 
 app.use('/api/movies', movieRoutes);
 
